017a432585158a02cbbd91295d271b20face2587
Mike DeVine <pixelshredder@gmail.com>
2017-06-07 23:22:53 -0400

    I'm.... really bad at incremental code updates.

--- js/howdoipoliticsmain.js
+++ js/howdoipoliticsmain.js
@@ -1,4 +1,3 @@
-var places = [];
 var placeTypes = {
 	'neighborhood': {
 		'type_label': "Neighborhood",
@@ -6,7 +5,7 @@ var placeTypes = {
 	},
 	'locality': {
 		'type_label': "Town/City",
-		'address_components': [0, 2]
+		'address_components': [0, 3]
 	},
 	'administrative_area_level_2': {
 		'type_label': "County",
@@ -21,29 +20,398 @@ var placeTypes = {
 		'address_components': [0, 1]
 	}
 }
-var oReq = new XMLHttpRequest();
-
-function reqListener() {
-	console.log(this.responseText);
-}
-
-oReq.onload = function() {
-	alert(this.responseText);
+var states = {
+	'AL' : {
+		'name': 'Alabama',
+		'type': 'State'
+	},
+	'AK': {
+		'name': 'Alaska',
+		'type': 'State'
+	},
+	'AS': {
+		'name': 'American Samoa',
+		'type': 'Territory'
+	},
+	'AZ': {
+		'name': 'Arizona',
+		'type': 'State'
+	},
+	'AR': {
+		'name': 'Arkansas',
+		'type': 'State'
+	},
+	'CA': {
+		'name': 'California',
+		'type': 'State'
+	},
+	'CO': {
+		'name': 'Colorado',
+		'type': 'State'
+	},
+	'CT': {
+		'name': 'Connecticut',
+		'type': 'State'
+	},
+	'DE': {
+		'name': 'Delaware',
+		'type': 'State'
+	},
+	'DC': {
+		'name': 'District Of Columbia',
+		'type': 'District'
+	},
+	'FL': {
+		'name': 'Florida',
+		'type': 'State'
+	},
+	'GA': {
+		'name': 'Georgia',
+		'type': 'State'
+	},
+	'GU': {
+		'name': 'Guam',
+		'type': 'Territory'
+	},
+	'HI': {
+		'name': 'Hawaii',
+		'type': 'State'
+	},
+	'ID': {
+		'name': 'Idaho',
+		'type': 'State'
+	},
+	'IL': {
+		'name': 'Illinois',
+		'type': 'State'
+	},
+	'IN': {
+		'name': 'Indiana',
+		'type': 'State'
+	},
+	'IA': {
+		'name': 'Iowa',
+		'type': 'State'
+	},
+	'KS': {
+		'name': 'Kansas',
+		'type': 'State'
+	},
+	'KY': {
+		'name': 'Kentucky',
+		'type': 'State'
+	},
+	'LA': {
+		'name': 'Louisiana',
+		'type': 'State'
+	},
+	'ME': {
+		'name': 'Maine',
+		'type': 'State'
+	},
+	'MD': {
+		'name': 'Maryland',
+		'type': 'State'
+	},
+	'MA': {
+		'name': 'Massachusetts',
+		'type': 'State'
+	},
+	'MI': {
+		'name': 'Michigan',
+		'type': 'State'
+	},
+	'MN': {
+		'name': 'Minnesota',
+		'type': 'State'
+	},
+	'MS': {
+		'name': 'Mississippi',
+		'type': 'State'
+	},
+	'MO': {
+		'name': 'Missouri',
+		'type': 'State'
+	},
+	'MT': {
+		'name': 'Montana',
+		'type': 'State'
+	},
+	'NE': {
+		'name': 'Nebraska',
+		'type': 'State'
+	},
+	'NV': {
+		'name': 'Nevada',
+		'type': 'State'
+	},
+	'NH': {
+		'name': 'New Hampshire',
+		'type': 'State'
+	},
+	'NJ': {
+		'name': 'New Jersey',
+		'type': 'State'
+	},
+	'NM': {
+		'name': 'New Mexico',
+		'type': 'State'
+	},
+	'NY': {
+		'name': 'New York',
+		'type': 'State'
+	},
+	'NC': {
+		'name': 'North Carolina',
+		'type': 'State'
+	},
+	'ND': {
+		'name': 'North Dakota',
+		'type': 'State'
+	},
+	'MP': {
+		'name': 'Northern Mariana Islands',
+		'type': 'Commonwealth'
+	},
+	'OH': {
+		'name': 'Ohio',
+		'type': 'State'
+	},
+	'OK': {
+		'name': 'Oklahoma',
+		'type': 'State'
+	},
+	'OR': {
+		'name': 'Oregon',
+		'type': 'State'
+	},
+	'PA': {
+		'name': 'Pennsylvania',
+		'type': 'State'
+	},
+	'PR': {
+		'name': 'Puerto Rico',
+		'type': 'Commonwealth'
+	},
+	'RI': {
+		'name': 'Rhode Island',
+		'type': 'State'
+	},
+	'SC': {
+		'name': 'South Carolina',
+		'type': 'State'
+	},
+	'SD': {
+		'name': 'South Dakota',
+		'type': 'State'
+	},
+	'TN': {
+		'name': 'Tennessee',
+		'type': 'State'
+	},
+	'TX': {
+		'name': 'Texas',
+		'type': 'State'
+	},
+	'UT': {
+		'name': 'Utah',
+		'type': 'State'
+	},
+	'VT': {
+		'name': 'Vermont',
+		'type': 'State'
+	},
+	'VI': {
+		'name': 'Virgin Islands',
+		'type': 'Territory'
+	},
+	'VA': {
+		'name': 'Virginia',
+		'type': 'State'
+	},
+	'WA': {
+		'name': 'Washington',
+		'type': 'State'
+	},
+	'WV': {
+		'name': 'West Virginia',
+		'type': 'State'
+	},
+	'WI': {
+		'name': 'Wisconsin',
+		'type': 'State'
+	},
+	'WY': {
+		'name': 'Wyoming',
+		'type': 'State'
+	}
 };
 
-oReq.open("get", "get-data.php", true);
-oReq.send();
+var searchTweaks = [
+	{
+		'term': ', VA,',
+		'tweak': ' -west+virginia'
+	},
+	{
+		'term': 'Virginia,',
+		'tweak': ' -west+virginia'
+	}
+];
 
-function parseResults(results) {
+$(document).ready(function() {
+	initFacebook();
+	getLocation();
+});
+
+function initFacebook() {
+	$.ajaxSetup({ cache: true });
+	$.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+		FB.init({
+			appId: '301393976977338',
+			version: 'v2.7'
+		});     
+		$('#loginbutton,#feedbutton').removeAttr('disabled');
+		//FB.getLoginStatus(updateStatusCallback);
+	});
+}
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
+	} else {
+		showError({
+			code: 'legacy'
+		});
+		loadForm();
+	}
+}
+
+function loadForm() {
+	$('.search').append(
+		$("<h3/>").text("Contact Form"),
+		$("<p/>").text("This is my form. Please fill it out. It's awesome!"),
+		$("<form/>", {
+			id: 'location',
+			name: 'location'
+		}).append(
+			$("<input/>", {
+				type: 'text',
+				id: 'address1',
+				name: 'address1',
+				placeholder: 'Street Address, Line 1',
+				required: true
+			}),
+			$("<input/>", {
+				type: 'text',
+				id: 'address2',
+				name: 'address2',
+				placeholder: 'Street Address, Line 2'
+			}),
+			$("<input/>", {
+				type: 'text',
+				id: 'city',
+				name: 'city',
+				placeholder: 'City/Town',
+				required: true
+			}),
+			$("<select/>", {
+				id: 'state',
+				name: 'state',
+				placeholder: 'State',
+				required: true
+			}),
+			$("<br/>"),
+			$("<input/>", {
+				type: 'submit',
+				id: 'submit',
+				value: 'Submit'
+			})
+		)
+	);
+	$('#state').append(
+		$("<option>", {
+			value: '',
+			text: 'Select a state/territory',
+			selected: true
+		})
+	);
+	$.each(states, function(key, value) {
+		$('#state').append(
+			$("<option>", {
+				value: this.key,
+				text: this.value.name
+			})
+		);
+	});
+	$('#state').change(function(){}).trigger('change');
+	$('#location').on('submit', function(e) {
+		e.preventDefault();
+		clearResults();
+		processForm($(this));
+	});
+}
+
+function processForm(form) {
+	var position;
+	var address = form.children('input[name="address1"]').val();
+	var address2 = form.children('input[name="address2"]').val();
+	if (address2 !== '') {
+		address = address + ',+' + address2;
+	}
+	address = address + ',+' + form.children('input[name="city"]').val();
+	address = address + ',+' + form.children('#state').val();
+	address = address.replace(/ /g, "_").replace(/\//g, "_").toLowerCase();
+	$.ajax({
+		url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyBO4ShgmmgpAeZfCcu8YYTZZ04i0vxR4DA'
+	}).then(function(data) {
+		if (data.status === 'ZERO_RESULTS') {
+			showError('noformresults');
+			return;
+		}
+		position = {
+			coords: {
+				latitude: data.results[0].geometry.location.lat,
+				longitude: data.results[0].geometry.location.lng
+			}
+		};
+		showPosition(position);
+	});
+};
+
+function clearResults() {
+	$('.results').empty();
+}
+
+function showPosition(position) {
+	var districts = [];
+	$.ajax({
+		url: 'https://api.geocod.io/v1/reverse?q=' + position.coords.latitude + ',' + position.coords.longitude + '&fields=stateleg&api_key=cc8e7cb67fc566e98de4fd9b56dbb922788f52f'
+	}).then(function(data) {
+		if (data.results[0].fields.state_legislative_districts) {
+			districts = data.results[0].fields.state_legislative_districts;
+		}
+		$.ajax({
+			url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyBO4ShgmmgpAeZfCcu8YYTZZ04i0vxR4DA'
+		}).then(function(data) {
+			parseResults(data.results, districts);
+		});
+	});
+}
+
+function parseResults(results, districts) {
 	var result;
+	var places = [];
 	var place;
 	var id;
 	var label;
 	var component1;
 	var component2;
+	var searchTweak;
+	var typeLabel;
+	var state = '';
+
 	for (var i = 0; i < results.length; i++) {
 		result = results[i];
-		//console.log(result);
 		if (placeTypes[result.types[0]]) {
 			place = {};
 			component1 = result.address_components[placeTypes[result.types[0]].address_components[0]].long_name;
@@ -51,44 +419,71 @@ function parseResults(results) {
 			if (result.types[0] === 'political') {
 				id = id + '_' + i;
 			}
-			if ((result.types[0] === 'administrative_area_level_1') && (result.address_components[placeTypes[result.types[0]].address_components[1]].short_name != result.address_components[placeTypes[result.types[0]].address_components[1]].long_name)) {
+
+			if ((result.types[0] === 'administrative_area_level_1') && (result.address_components[0].short_name in states)) {
+				typeLabel = states[result.address_components[0].short_name]['type'];
+			} else {
+				typeLabel = placeTypes[result.types[0]].type_label;
+			}
+
+			state = result.address_components[placeTypes[result.types[0]].address_components[1]].long_name;
+
+			if ((result.types[0] === 'administrative_area_level_1') && 
+			(result.address_components[0].short_name in states) && 
+			(states[result.address_components[0].short_name]['type'] === 'State')) {
 				component2 = '+state';
 			} else {
 				component2 = ',+' + result.address_components[placeTypes[result.types[0]].address_components[1]].short_name;
 			}
+			searchTweak = getSearchTweaks(result);
 			place = {
 				'id': id,
-				'type_label': placeTypes[result.types[0]].type_label,
+				'type_label': typeLabel,
 				'label': component1,
-				'search_string': component1 + component2
+				'search_string': component1 + component2,
+				'search_tweak': searchTweak
 			};
 			places.push(place);
 		}
 	}
-	addDistrictPlaces(places);
-	//console.log(places);
+
+	(districts['senate'] && (state !== '')) ? addDistrictPlaces(places, districts, state) : loadResultsBoxes(places);
 }
-function addDistrictPlaces(places) {
-	/*$.ajax({
-		url: 'https://congress.api.sunlightfoundation.com/districts/locate?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude + '&apikey='
-	}).then(function(data) {
+
+function getSearchTweaks(result) {
+	var searchTweak;
+	for (var i = 0; i < searchTweaks.length; i++) {
+		searchTweak = searchTweaks[i];
+		if (result.formatted_address.indexOf(searchTweak.term) > -1) {
+			return searchTweak.tweak;
+		}
+	}
+	return '';
+}
+
+function addDistrictPlaces(places, districts, state) {
+	var type_label;
+	$.each(districts, function(key, value) {
+		type_label = value.name.replace(' ' + value.district_number, '').replace('District', 'Legislative District');
 		place = {
-			'id': 'state_congressional_district',
-			'type_label': 'State Congressional District (CD)',
-			'label': component1,
-			'search_string': data.state + data.district + "+District"
+			'id': value.name.replace(/ /g, "_").replace(/\//g, "_").toLowerCase(),
+			'type_label': type_label,
+			'label': value.district_number,
+			'search_string': state + '+' + value.name
 		};
-		places.push(place);*/
-		loadResultsBoxes(places);
-	//});
+		places.push(place);
+	});
+
+	loadResultsBoxes(places);
 }
+
 function loadResultsBoxes(places) {
 	var place;
 	var q;
 	$('.results').append('<h3>We Found Some Democratic Organizations:</h3>');
 	for (var i = 0; i < places.length; i++) {
 		place = places[i];
-		q = encodeURIComponent('"' + place.search_string + '" democratic organization');
+		q = encodeURIComponent('"' + place.search_string + '" democratic organization' + place.search_tweak);
 		$('.results').append('<h4>In Your ' + place.type_label + ' (' + place.label + ')</h4>');
 		$('.results').append('<table id="' + place.id + '_results_table"></table>');
 		$('#' + place.id + '_results_table').append('<tr id="' + place.id + '_results_row' + '"></tr>');
@@ -96,6 +491,7 @@ function loadResultsBoxes(places) {
 		loadGoogleResultsBox(place, q);
 	}
 }
+
 function loadGoogleResultsBox(place, q) {
 	$('#' + place.id + '_results_row').append('<td id="' + place.id + '_google_results"></td>');
 	$('<iframe>', {
@@ -105,8 +501,9 @@ function loadGoogleResultsBox(place, q) {
 		scrolling: 'yes'
 	}).appendTo('#' + place.id + '_google_results');
 }
+
 function loadFacebookResultsBox(place, q) {
-	var urlCall = '/search?q=' + q + '&type=page&fields=id,name,about,contact_address,description,engagement,general_info,link,phone,single_line_address,website&access_token=EAACEdEose0cBAJQGW2gYd7jZBZB6zZArlPYUl20qHgRgzofScOJAPZBiPAAawDBJ5cmQdM1417578qYk4mINgHgz8fU2yyr6NCZAeZBcm6ZAxj72CvEaq2BxTJUYaP5TSpWKZBURAiZAECaHb7jmUxTzmHjyO2CWO2sGt14yctMB1C7H3XklID6wD3SvPco5nccsZD';
+	var urlCall = '/search?q=' + q + '&type=page&fields=id,name,about,contact_address,description,engagement,general_info,link,phone,single_line_address,website&access_token=301393976977338|o7BuFDcKxXWAL3i9b3T_2jJgfT4';
 	var blurb;
 	var engagement;
 	var contact;
@@ -127,7 +524,11 @@ function loadFacebookResultsBox(place, q) {
 					}
 					engagement = '';
 					if (response.data[i].engagement) {
-						engagement = '<p>' + response.data[i].engagement.social_sentence + '</p>';
+						if (response.data[i].engagement.social_sentence === 'Be the first of your friends to like this.') {
+							engagement = '<p>No one likes this Facebook page yet.</p>';
+						} else {
+							engagement = '<p>' + response.data[i].engagement.social_sentence.replace('.', ' ') + 'Facebook page.</p>';
+						}
 					}
 					contact = '';
 					if (
@@ -164,6 +565,7 @@ function loadFacebookResultsBox(place, q) {
 		}
 	);
 }
+
 function showError(error) {
 	switch(error.code) {
 		case error.PERMISSION_DENIED:
@@ -177,6 +579,12 @@ function showError(error) {
 			break;
 		case error.UNKNOWN_ERROR:
 			console.log("An unknown error occurred.");
+			break;
+		case 'legacy':
+			console.log("Geolocation is not supported by this browser.");
+			break;
+		case 'noformresults':
+			console.log("Geolocation is not supported by this browser.");
 			break;
 	}
 }
